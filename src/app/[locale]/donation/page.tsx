42f87004036f37ffd23978576a87d327f17dc23a
Nastya Byvalkevych <nastuly72@gmail.com>
2024-03-06 20:33:33 +0200

    get user info

--- src/app/[locale]/donation/page.tsx
+++ src/app/[locale]/donation/page.tsx
@@ -1,10 +1,16 @@
 import DonateSection from "@/components/sections/DonateSection";
+import { currentUser } from "@clerk/nextjs";
 
-export default function About() {
+export default async function About() {
+  const loggedInUserData = await currentUser();
+  // console.log(loggedInUserData);
   return (
     <div>
       <div className="px-4 md:px-16 flex flex-col gap-8 md:gap-20 ">
-        <DonateSection/>
+        <DonateSection />
+        <h1>Clerk ID:{loggedInUserData?.id}</h1>
+        <h1>Username:{loggedInUserData?.firstName}</h1>
+        <h1>Clerk ID:{loggedInUserData?.emailAddresses[0].emailAddress}</h1>
       </div>
     </div>
   );
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,6 @@
 import HeroSection from "@/components/sections/HeroSection";
-import { currentUser } from "@clerk/nextjs";
 
 export default function Home() {
-    const loggedInUserData = currentUser();
-    console.log(loggedInUserData);
+    
   return <HeroSection />;
 }

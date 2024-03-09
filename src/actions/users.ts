68f56857feb0d47b01927349b395aea3fd3280a9
Nastya Byvalkevych <nastuly72@gmail.com>
2024-03-09 18:20:41 +0200

    user model

--- src/actions/users.ts
+++ src/actions/users.ts
@@ -1,7 +1,38 @@
+"use server";
 import { connectDB } from "@/db/config";
+import UserModel from "@/models/user";
+import { currentUser } from "@clerk/nextjs";
 
-connectDB()
+connectDB();
 
-export const handleNewUserRegistration=async()=>{
+export const handleNewUserRegistration = async () => {
+  try {
+    const loggedInUserData = await currentUser();
 
-}
+    // check if the user model already exists , if its exits return user data
+    const existingUser = await UserModel.findOne({
+      clerkUserId: loggedInUserData?.id,
+    });
+    if (existingUser) return existingUser;
+
+    // create new user
+    let userName = loggedInUserData?.username;
+    if (!userName) {
+      userName = loggedInUserData?.firstName + " " + loggedInUserData?.lastName;
+      userName = userName?.replace("null", "");
+    }
+    const newUser = new UserModel({
+      clerkUserId: loggedInUserData?.id,
+      userName: userName,
+      email: loggedInUserData?.emailAddresses[0].emailAddress,
+      profilePic: loggedInUserData?.imageUrl,
+    });
+
+    await newUser.save();
+    return newUser;
+  } catch (error: any) {
+    return {
+      error: error.message,
+    };
+  }
+};

129b0b359bca979065ed1a47be8acbd8dbac64eb
nastyabyvalkevych <89982986+nastyabyvalkevych@users.noreply.github.com>
2025-05-17 13:29:29 +0300

    fix chatbot

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -6,6 +6,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import "remixicon/fonts/remixicon.css";
 import FooterSection from "@/components/common/Footer";
 import LayoutProvider from "@/providers/layout-provider";
+import Script from "next/script"; // Import Script from next/script
 
 const nunito = Nunito({
   subsets: ["latin"],
@@ -28,17 +29,56 @@ export default function RootLayout({
   params: { locale },
 }: Readonly<RootLayoutProps>) {
   const messages = useMessages();
+
   return (
     <ClerkProvider>
       <html lang={locale}>
         <body className={nunito.className}>
           <NextIntlClientProvider locale={locale} messages={messages}>
-            <LayoutProvider> {children}</LayoutProvider>
+            <LayoutProvider>{children}</LayoutProvider>
 
             <div className="mt-16 md:mt-32">
               <FooterSection />
             </div>
           </NextIntlClientProvider>
+
+          <Script
+            id="1"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                const iframe = document.createElement("iframe");
+                
+                const iframeStyles = (styleString) => {
+                  const style = document.createElement('style');
+                  style.textContent = styleString;
+                  document.head.append(style);
+                }
+                
+                iframeStyles(\`
+                  .chat-frame {
+                    position: fixed;
+                    bottom: 50px;
+                    right: 50px;
+                    border: none;
+                    backgroundColor:'white';
+                  }
+                \`);
+                
+                iframe.src = "https://diploma-sigma-weld.vercel.app/chatbot";
+                iframe.classList.add('chat-frame');
+                document.body.appendChild(iframe);
+                
+                window.addEventListener("message", (e) => {
+                  if (e.origin !== "https://diploma-sigma-weld.vercel.app") return;
+                  let dimensions = JSON.parse(e.data);
+                  iframe.width = dimensions.width;
+                  iframe.height = dimensions.height;
+                  iframe.contentWindow.postMessage("9a21c6fd-1da7-4338-bd04-1a56b680d077", "https://diploma-sigma-weld.vercel.app/");
+                });
+              `,
+            }}
+          />
         </body>
       </html>
     </ClerkProvider>
